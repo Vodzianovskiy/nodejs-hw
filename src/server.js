b7ff558f29d727e201815abfb4d06efa078803a4
Ivan Voddz <vvodzanovskij@gmail.com>
2026-05-21 13:20:07 +0200

    fix: add dotenv/config import

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
-import express from "express";
-import cors from "cors";
-import pino from "pino-http";
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+import pino from 'pino-http';
 
 const app = express();
 
@@ -10,19 +11,19 @@ app.use(express.json());
 app.use(cors());
 app.use(
   pino({
-    level: "info",
+    level: 'info',
     transport: {
-      target: "pino-pretty",
+      target: 'pino-pretty',
       options: {
         colorize: true,
-        translateTime: "HH:MM:ss",
-        ignore: "pid,hostname",
+        translateTime: 'HH:MM:ss',
+        ignore: 'pid,hostname',
         messageFormat:
-          "{req.method} {req.url} {res.statusCode} - {responseTime}ms",
+          '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
         hideObject: true,
       },
     },
-  }),
+  })
 );
 
 //middleware
@@ -32,19 +33,19 @@ app.use((req, res, next) => {
 });
 
 //test eror for backend
-app.get("/test-error", (req, res) => {
-  throw new Error("Simulated server error");
+app.get('/test-error', (req, res) => {
+  throw new Error('Simulated server error');
 });
 
 //all notes
-app.get("/notes", (req, res) => {
+app.get('/notes', (req, res) => {
   res.status(200).json({
-    message: "Retrieved all notes",
+    message: 'Retrieved all notes',
   });
 });
 
 //{id} notes
-app.get("/notes/:noteId", (req, res) => {
+app.get('/notes/:noteId', (req, res) => {
   const noteId = req.params.noteId;
   res.status(200).json({
     message: `Retrieved note with ID: ${noteId}`,
@@ -54,7 +55,7 @@ app.get("/notes/:noteId", (req, res) => {
 // middleware 404 {GET /profile | GET /payments}
 app.use((req, res) => {
   res.status(404).json({
-    message: "Route not found",
+    message: 'Route not found',
   });
 });
 
@@ -62,11 +63,11 @@ app.use((req, res) => {
 app.use((err, req, res, next) => {
   console.error(err);
 
-  const isProd = process.env.NODE_ENV === "production";
+  const isProd = process.env.NODE_ENV === 'production';
 
   res.status(500).json({
     message: isProd
-      ? "Something went wrong. Please try again later."
+      ? 'Something went wrong. Please try again later.'
       : err.message,
   });
 });
