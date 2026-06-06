7d3ef86c8ea93cc6f8bd6cc5f912593e745f99c8
Ivan Voddz <vvodzanovskij@gmail.com>
2026-06-06 14:02:11 +0200

    fix celebrate errors middleware in server.js

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
-import { isCelebrateError } from 'celebrate';
+import { errors } from 'celebrate';
 
 import { connectMongoDB } from './db/connectMongoDB.js';
 import { logger } from './middleware/logger.js';
@@ -15,37 +15,14 @@ const PORT = process.env.PORT ?? 3000;
 
 app.use(express.json());
 app.use(cors());
-
-// middleware
 app.use(logger);
 
-// routes
 app.use(notesRoutes);
 
-// 404 handler
 app.use(notFoundHandler);
-
-// celebrate error handler
-app.use((err, req, res, next) => {
-  if (isCelebrateError(err)) {
-    const errorMessages = [];
-    for (const [, joiError] of err.details.entries()) {
-      joiError.details.forEach((detail) => {
-        errorMessages.push(detail.message);
-      });
-    }
-    return res.status(400).json({
-      message: 'Validation error',
-      errors: errorMessages,
-    });
-  }
-  next(err);
-});
-
-// 500 error handler
+app.use(errors());
 app.use(errorHandler);
 
-// connect to MongoDB
 await connectMongoDB();
 
 app.listen(PORT, () => {
